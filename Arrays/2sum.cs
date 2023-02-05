de2d0c0a2f3260881da9969b944640fd7523fb6d
Folusayo <folusayo.abe@kuda.com>
2023-02-05 21:23:23 +0100

    Algoruthms

---
+++ Arrays/2sum.cs
@@ -0,0 +1,6 @@
+public class Test
+{
+    public int NumericValue { get; set; }
+    public bool BooleanValue { get; set; }
+    public string StringValue { get; set; }
+}
---
+++ Arrays/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var test = new Test();
+test.NumericValue = 12;
+test.StringValue = "New String";
+test.BooleanValue = true;
+System.Console.WriteLine(test);
