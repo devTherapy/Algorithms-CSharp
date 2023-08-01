da894a5134a9a609c72d1b02e9a1ecb074338d99
Folusayo <folusayo.abe@kuda.com>
2023-08-01 08:35:39 +0100

    node depth correction

--- Trees/NodeDepth.cs
+++ Trees/NodeDepth.cs
@@ -4,18 +4,18 @@ public static class NodeDepth
      public static int NodeDepths(BinaryTree root) 
      {
         var sumOfNodeDepths = 0;
-        var currentDepth = 0;
-         SumNodeDepths(root, currentDepth, ref sumOfNodeDepths);
+        var currentNodeDepth = 0;
+         SumNodeDepths(root, currentNodeDepth, ref sumOfNodeDepths);
         // Write your code here.
          return sumOfNodeDepths;
      }
 
-    private static void SumNodeDepths(BinaryTree node, int currentDepth, ref int sumOfNodeDepths)
+    private static void SumNodeDepths(BinaryTree node, int currentNodeDepth, ref int sumOfNodeDepths)
     {
         if(node == null) return;
-        sumOfNodeDepths = currentDepth + 1;
-        SumNodeDepths(node.left, currentDepth + 1, ref sumOfNodeDepths);
-        SumNodeDepths(node.right, currentDepth + 1, ref sumOfNodeDepths);
+        sumOfNodeDepths += currentNodeDepth;
+        SumNodeDepths(node.left, currentNodeDepth + 1, ref sumOfNodeDepths);
+        SumNodeDepths(node.right, currentNodeDepth + 1, ref sumOfNodeDepths);
 
     }
 }
