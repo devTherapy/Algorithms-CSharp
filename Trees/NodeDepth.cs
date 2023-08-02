99870bb93801bd678888f3af65167c1a5e0d5a19
Folusayo <folusayo.abe@kuda.com>
2023-08-02 08:12:46 +0100

    node depth iterative solution

--- Trees/NodeDepth.cs
+++ Trees/NodeDepth.cs
@@ -1,6 +1,7 @@
 public static class NodeDepth
 {
 
+    //recursion solution
      public static int NodeDepths(BinaryTree root) 
      {
         var sumOfNodeDepths = 0;
@@ -18,4 +19,22 @@ public static class NodeDepth
         SumNodeDepths(node.right, currentNodeDepth + 1, ref sumOfNodeDepths);
 
     }
+
+    //Iterative approach
+    public static int NodeDepths_Iterative(BinaryTree root) 
+    {
+        var sumOfNodeDepths = 0;
+        var stack =  new Stack<(BinaryTree node, int nodeDepth)>();
+        stack.Push((root, 0));
+        while (stack.Count > 0)
+        {
+            var (node, nodeDepth) = stack.Pop();
+            sumOfNodeDepths += nodeDepth;
+            if(node.left is not null) stack.Push((node.left, nodeDepth + 1));
+            if(node.right is not null) stack.Push((node.right, nodeDepth + 1));
+        }
+        return sumOfNodeDepths;
+    }
+
+
 }
