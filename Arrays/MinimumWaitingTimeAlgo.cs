f5cf4787f9fce721c866462544dcec4c2cd59335
Folusayo <folusayo.abe@kuda.com>
2023-08-04 11:43:28 +0100

    minimum waiting time

---
+++ Arrays/MinimumWaitingTimeAlgo.cs
@@ -0,0 +1,9 @@
+public class MinimumWaitingTimeAlgo{
+     public int MinimumWaitingTime(int[] queries) 
+     {
+          queries.
+       // Write your code here.
+       
+      return -1;
+     }
+}
