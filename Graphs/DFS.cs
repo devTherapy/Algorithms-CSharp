efe285ae5f318f8dfdf42a5ae2e1feaa121e702d
Folusayo <folusayo.abe@kuda.com>
2023-08-04 10:04:27 +0100

    DFS graphs

---
+++ Graphs/DFS.cs
@@ -0,0 +1,26 @@
+﻿namespace Graphs;
+public class Node
+{
+        public string name;
+        public List<Node> children = new List<Node>();
+
+        public Node(string name) {
+        this.name = name;
+        }
+
+    public List<string> DepthFirstSearch(List<string> array) {
+      // Write your code here.
+      array.Add(name);
+      foreach (var child in children)
+      {
+        child.DepthFirstSearch(array);
+      }
+      return array;
+    }
+
+    public Node AddChild(string name) {
+      Node child = new Node(name);
+      children.Add(child);
+      return this;
+    }
+}
