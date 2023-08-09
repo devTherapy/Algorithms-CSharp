ca69019eb6f0bc59e692c74f26ff71564f82f087
Folusayo <folusayo.abe@kuda.com>
2023-08-09 07:38:56 +0100

    Remove duplicate from linkedlist

---
+++ LinkedLists/Class1.cs
@@ -0,0 +1,28 @@
+﻿namespace LinkedLists
+
+public class LinkedList {
+    public int value;
+    public LinkedList next;
+
+    public LinkedList(int value) {
+      this.value = value;
+      this.next = null;
+    }
+  }
+
+  public LinkedList RemoveDuplicatesFromLinkedList(LinkedList linkedList) {
+    // Write your code here.
+    var current = linkedList;
+    while (current.next is not null)
+    {
+        if (current.value == current.next.value)
+        {
+            current.next = current.next.next
+        }
+        else
+        {
+            current = current.next
+        }
+    }
+    return linkedList;
+  }
