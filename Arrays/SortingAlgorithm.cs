4daf2d028f50811e0176f49cf65b1ff204857c69
Folusayo <folusayo.abe@kuda.com>
2023-08-16 07:11:24 +0100

    sorting algorithms

--- Arrays/SortingAlgorithm.cs
+++ Arrays/SortingAlgorithm.cs
@@ -18,4 +18,37 @@ public class SortingAlgo
     }
     return array;
   }
+
+    public static int[] InsertionSort(int[] array) {
+    // Write your code here.
+    for (int i = 1; i < array.Length; i++)
+    {
+        var j = i;
+        while (j > 0 && array[j] < array[j - 1])
+        {
+                (array[j], array[j - 1]) = (array[j - 1], array[j]);
+                j--;        
+        }
+    }
+    return array;
+  }
+
+   public static int[] SelectionSort(int[] array)
+    {
+         // Write your code here.
+         for (int i = 0; i < array.Length - 1; i++)
+         {
+            var minIndex = i;
+
+            for (int j = i + 1; j < array.Length; j++)
+            {
+                if (array[j] < array[minIndex])
+                {
+                    minIndex = j;
+                }
+            }
+            (array[minIndex], array[i]) = (array[i], array[minIndex]);
+        }
+        return array;
+    }
 }
