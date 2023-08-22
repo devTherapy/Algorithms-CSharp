57fabea6a92e7a6d21d290a539ff98cb478d9489
Folusayo <folusayo.abe@kuda.com>
2023-08-22 05:33:05 +0100

    more algos

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -2,10 +2,5 @@
 //var inputArray = new int[]{-10, -5, 0, 5, 10};
 //var ans = SortedSquare.SortedSquaredArray(inputArray);
 
-var array = new List<int>{5, 1, 22, 25, 6, -1, 8, 10};
-var sequence = new List<int>{22, 25, 6};
-// [2, [7, 6, [5]]]
-var objects = new List<object>{2, new List<object>{7, 6, new List<object>{5}}};
-
-ProductSumAlgo.ProductSum(objects);
+using Microsoft.VisualBasic;
 //System.Console.WriteLine(ans);
---
+++ Strings/Class1.cs
@@ -0,0 +1,84 @@
+﻿using System.Text;
+namespace Strings;
+public static class StringsAlgo
+{
+  public static bool IsPalindrome(string str) {
+    // Write your code here.
+    var left = 0;
+    var right = str.Length - 1;
+
+  while (left <= right)
+  {
+    if (str[left] != str[right]) return false;
+    left++;
+    right--;
+  }
+    return true;
+  }
+
+   public static string CaesarCypherEncryptor(string str, int key) {
+    // Write your code here.
+    var builder = new StringBuilder();
+    foreach (var character in str)
+    {
+      var asciiValue = character + (key % 26); 
+      var shiftedCharacter = (char)asciiValue;
+      if (shiftedCharacter > 'z')
+      {
+        shiftedCharacter = (char)('a' - 1 + (shiftedCharacter % 'z'));
+      }
+      builder.Append(shiftedCharacter);
+    }
+    return builder.ToString();
+  }
+
+  public static string RunLengthEncoding(string str) {
+    // Write your code here.
+    var currChar = str[0];
+    var count = 0;
+    var builder = new StringBuilder();
+
+    foreach (var character in str)
+    {
+       if (currChar != character || count == 9)
+       {
+          builder.Append(count);
+          builder.Append(currChar);
+          currChar = character;
+          count = 0;
+       }
+       count++;
+    }
+    builder.Append(count);
+    builder.Append(currChar);
+    return builder.ToString();
+  }
+
+  public static string[] CommonCharacters(string[] strings) {
+    // Write your code here.
+    var has = new HashSet<string>();
+    var dict = new Dictionary<char, int>();
+    var list = new List<string>();
+    for (int i = 0; i < strings.Length; i++)
+    {
+      var currStr = strings[i];
+      var set = currStr.ToHashSet();
+      foreach (var character in set)
+      {
+        if (dict.ContainsKey(character))
+        {
+          dict[character] += 1;
+          if (dict[character] == strings.Length)
+          {
+            list.Add(character.ToString());
+          }
+        }
+        else
+        {
+          dict.Add(character, 1);
+        }
+      }
+    }
+    return list.ToArray();
+  }
+}
